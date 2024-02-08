0215483363aa14eb040539df3b98e5a7b3e0fdec
welton-2020 <weltdias@gmail.com>
2024-02-08 20:45:26 -0300

    Add o pom.xml, application.properties and Startup.java

---
+++ do-zero-a-aws/src/test/java/br/com/welton/dozeroaaws/DoZeroAAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.welton.dozeroaaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoZeroAAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
